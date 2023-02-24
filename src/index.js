0361686e9f361e91d4106bc6a318655a99debf17
4dambarr <ab2474@york.ac.uk>
2023-02-24 16:51:43 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = () => {
+  const verbs = [
+    "Made",
+    "Crafted",
+    "Created",
+    "Coded",
+    "Developed",
+    "Designed",
+    "Engineered",
+    "Fabricated",
+    "Formed",
+    "Programmed",
+    "Produced",
+    "Built",
+    "Assembled",
+    "Constructed",
+  ]
+
+  const adverb = [
+    "care ",
+    "love ",
+    "passion ",
+    "heart ",
+    "soul ",
+    "blood, sweat, and tears ",
+    "desire "
+  ]
+
+  return <div className={styles.test}>
+      &copy; {new Date().getFullYear()} - 
+      {verbs[Math.floor(Math.random() * verbs.length)]} with {adverb[Math.floor(Math.random() * adverb.length)] }
+      by <a href="https://adam-barr.me" target="_blank" rel="noopener noreferrer">Adam</a>
+  </div>
 }
